75226d86f131b89af63cf81f08ade900f42c67c9
MD. MUARIF HASNAT RAKIB <mhrakib20025@gmail.com>
2026-09-21 16:44:46 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,26 @@
+// Create web server
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading index.html');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Page not found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
+
